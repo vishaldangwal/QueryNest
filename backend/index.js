347bc804cb6caaa144149301f49e3d0e2fdff771
Vipin Kumar Chauhan <vipinchauhan2451@gmail.com>
2025-02-25 21:52:54 +0530

    fixing typo

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
